6f01744af5eba3fc604d094531f1519f1e1bc94c
Matthew van Bird <mbird1989@hotmail.com>
2025-12-09 22:54:13 +0000

    day8

---
+++ 2025/csharp/Runner/Day8/Day8.cs
@@ -0,0 +1,18 @@
+﻿using Common;
+
+namespace Day8;
+
+public class Day8 : IDay
+{
+    private string[] inputLines = File.ReadAllLines("input.txt"); 
+    
+    public string Part1()
+    {
+        throw new NotImplementedException();
+    }
+
+    public string Part2()
+    {
+        throw new NotImplementedException();
+    }
+}
